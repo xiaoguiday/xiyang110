50b7e0a08788f4f89442b0a84707c9bdc2407620
xiaoguiday <133844933+xiaoguiday@users.noreply.github.com>
2025-10-18 23:14:58 +0800

    Update main.go

--- main.go
+++ main.go
@@ -27,7 +27,7 @@ import (
 	"syscall"
 	"time"
 
-	"github.comcom/shirou/gopsutil/v3/cpu"
+	"github.com/shirou/gopsutil/v3/cpu"
 	"github.com/shirou/gopsutil/v3/mem"
 	"golang.org/x/crypto/bcrypt"
 )
